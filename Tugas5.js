c12bf4446ad362629ad6fc2e5e190c7d46c40cb9
RatnaPujiPratiwi1 <73380924+RatnaPujiPratiwi1@users.noreply.github.com>
2020-10-24 21:20:44 +0700

    Add files via upload

---
+++ Tugas5.js
@@ -0,0 +1,48 @@
+// 1. Array dan Object
+//    Array adalah struktur data untuk menyimpan sekumpulan data 
+//    Object adalah variabel yang menyimpan nilai dan method
+
+// 2. Variabel hobi menggunakan Array
+//    var hobi = [ "Memasak", "Menonton Film Horror", "Membaca" ];
+
+// 3. Variabel Data Diri
+//    var datadiri = {
+//	      nama : "Ratna",
+//  	  umur : "23",
+//		  alamat : " Jalan Raya Inpres Gg.Generasi II Jakarta Timur",
+//		  hobi : "Memasak";
+//		  }
+// 4. Variabel Array of Object
+//    const sekolah = [
+//    {
+//      namaSekolah:"Tk Muslimat",
+//		jurusan :"-",
+//		tahunMasuk : "2002"
+//		tahunKeluar : "2003"
+//	  },
+//	  {namaSekolah:" SDN GETAS",
+//		jurusan :"-",
+//		tahunMasuk : "2003"
+//		tahunKeluar : "2009"
+//	  },
+//	  namaSekolah:"SMPN 1 NEGERI",
+//		jurusan :"-",
+//		tahunMasuk : "2009"
+//		tahunKeluar : "2012"
+//	  },
+//	  namaSekolah:"SMKN I CEPU",
+//		jurusan :"Administrasi Perkantoran",
+//		tahunMasuk : "2012"
+//		tahunKeluar : "2015"
+//	  },
+//	  namaSekolah:"UNIVERSITAS INDRAPRASTA",
+//		jurusan :"Informatika",
+//		tahunMasuk : "2016"
+//		tahunKeluar : "2020"
+//	  ]
+
+// 5. Perbedaan kutip 1(') dan kutip 2 ("")
+//    const jawaban =" kutip satu digunakan deklarasi string dan tidak dapat
+//    menampilkan otomatis isi variabel didalamnya "
+//    const jawaban =" kutip dua digunakan deklarasi string dan menampilkan 
+//    otomatis isi variabel didalamnya "
