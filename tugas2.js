e2b39974f2cee6e648a4f87853fce980e2c76ff1
RatnaPujiPratiwi1 <73380924+RatnaPujiPratiwi1@users.noreply.github.com>
2020-10-24 20:09:57 +0700

    Add files via upload

---
+++ tugas2.js
@@ -0,0 +1,60 @@
+// 1. Buatlah beberapa variabel di JavaScript
+ <!DOCTYPE html>
+<html>
+<head>
+	<title>  Console </title>
+</head>
+<body>
+	Using the Console 
+<script>
+var nama="Ratna Puji Pratiwi";
+let umur="23";
+var alamat="Jakarta Timur";
+var hobi="Memasak";
+var asal ="Universitas Indraprasta PGRI";
+var alasanSukaProgramming ="ingin memperdalam ilmu programming dan untuk mengetahui teknologi terupdate ";
+
+</script>
+</body>
+</html>
+
+// 2. Console.log gabungan
+<!DOCTYPE html>
+<html>
+<head>
+	<title>  Console </title>
+</head>
+<body>
+	Using the Console 
+<script>
+
+var nama="Ratna Puji Pratiwi";
+var asalUniversitas ="Universitas Indraprasta PGRI";
+var jurusan ="Universitas Indraprasta PGRI";
+var alasanSukaProgramming ="ingin memperdalam ilmu programming dan untuk mengetahui teknologi terupdate ";
+console.log("Nama saya adalah:"+nama);
+console.log("asal universitas:"+asalUniversitas);
+console.log("jurusan:"+jurusan);
+console.log("Alasan saya tertarik dengan programming adalah:"+alasanSukaProgramming;
+
+</script>
+</body>
+</html>
+
+// 3. Jelaskan perbedaan var, let dan const 
+<!DOCTYPE html>
+<html>
+<head>
+	<title>  Console </title>
+</head>
+<body>
+	Using the Console 
+<script>
+
+console.log(" Perbedaan var, let dan const adalah let dan var dapat diubah isinya kembali sedangkan
+			  const tidak bisa";
+
+</script>
+</body>
+</html>
+   
---
+++ tugas3.js
@@ -0,0 +1,53 @@
+1. Buatlah 2 comment diJavaScript
+    // ini comment satu baris
+	/* Tipe data di JavaScript
+       1. String contoh nama orang, nama tempat
+	   2. Integer atau Number (Bilangan Bulat) contoh 0,1,2,3,4,5 dst
+	   3. Float( Bilangan Pecahan) 
+	   4. Boolean contoh true or false
+	   5. Object adalah tipe data yang bersifat mutable, artinya data ini
+	      dapat dimanipulasi berdasarkan method yang disediakan pada tipe data object */
+2. Deklarasikan dan assign semua tipe data di JavaScript
+   // Tipe data String diJavaScript
+      const namaDepan =" Ratna " ;
+      const namaBelakang =" Pratiwi " ;
+	  let alamat = "Jalan Raya Inpres Gg. Generasi II Jakarta Timur"
+   // Tipe data Integer di JavaScript
+      var umur  = "23";
+	  var angka = "20";
+	  var telepon = "088223934801"
+   // Tipe data Boolean di JavaScript
+	  var single = true ;
+	  var a = false ;
+	  var x = a<b ;
+3. Deklarasikan dan assign 3 variabel 
+   // Penjumlahan
+      const angkaPertama = 12
+	  const angkaKedua = 15
+	  let total 
+	  
+	  total = angkaPertama + angkaKedua ;
+	  console.log(total) ;
+	  
+   // Pengurangan
+      var angkaPertama = 120
+	  var angkaKedua = 100
+	  var total = angkaPertama - angkaKedua ;
+	  console.log(total) ;
+	  
+   // Pembagian
+      const angkaPertama = 12
+	  const angkaKedua = 15
+	  let total 
+	  
+	  total = angkaPertama / angkaKedua ;
+	  console.log(total) ;
+	  
+   // Perkalian 
+	  const angkaPertama = 12
+	  const angkaKedua = 15
+	  let total 
+	  
+	  total = angkaPertama *  angkaKedua ;
+	  console.log(total) ;
+	  
